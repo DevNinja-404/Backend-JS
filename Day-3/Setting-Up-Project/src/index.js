bc563e2be6e1d8e7f19bcc1a374755140fb19666
Bikash <sahb2676@gmail.com>
2024-06-24 16:03:16 +0545

    Setup The Project and Connected DB

---
+++ Day-3/Setting-Up-Project/src/index.js
@@ -0,0 +1,31 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+dotenv.config({ path: "./env" });
+
+// Second Approach :-
+import connectDB from "./db/index.js";
+connectDB();
+
+// First Approach :-
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from "express";
+// const app = express();
+// ;(async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+
+//     // well what if DB is connected but our app is not able to communicate with it so to check that we use the code below :
+//     app.on("error", (error) => {
+//       console.log("App not able to communicate with DB!!!");
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App running at URL : http:\\localhost:${process.env.PORT}`);
+//     });
+//   } catch (err) {
+//     console.error("ERROR : ", err);
+//     throw err;
+//   }
+// })();
