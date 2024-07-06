475f1720256e637cbdc5483ce64917d6427317e7
Bikash <sahb2676@gmail.com>
2024-07-06 11:50:19 +0545

    Added Pagination and Search,Sort option to get all videos

--- Day-3/Setting-Up-Project/src/routes/video.routes.js
+++ Day-3/Setting-Up-Project/src/routes/video.routes.js
@@ -5,10 +5,11 @@ import { upload } from "../middlewares/multer.middleware.js";
 import { Router } from "express";
 const router = Router();
 
-// router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file after all only loggedIn user can CRUD video
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file after all only loggedIn user can CRUD video
+
+router.route("/").get(videoController.getAllVideos);
 
 router.route("/upload-video").post(
-  verifyJWT,
   upload.fields([
     { name: "videoFile", maxCount: 1 },
     { name: "thumbnail", maxCount: 1 },
@@ -16,20 +17,16 @@ router.route("/upload-video").post(
   videoController.publishAVideo
 );
 
-router.route("/:videoId").get(verifyJWT, videoController.getVideoById);
-router.route("/update/:videoId").patch(verifyJWT, videoController.updateVideo);
+router.route("/:videoId").get(videoController.getVideoById);
+router.route("/update/:videoId").patch(videoController.updateVideo);
 
 router
   .route("/update-thumbnail/:videoId")
-  .patch(
-    verifyJWT,
-    upload.single("thumbnail"),
-    videoController.updateVideoThumbnail
-  );
+  .patch(upload.single("thumbnail"), videoController.updateVideoThumbnail);
 
-router.route("/delete/:videoId").delete(verifyJWT, videoController.deleteVideo);
+router.route("/delete/:videoId").delete(videoController.deleteVideo);
 
 router
   .route("/toggle-published/:videoId")
-  .patch(verifyJWT, videoController.togglePublishedStatus);
+  .patch(videoController.togglePublishedStatus);
 export default router;
